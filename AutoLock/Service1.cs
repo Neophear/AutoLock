980df806726a760e32ad5b5f4595fb435e886061
Stiig Gade <stiiggade@gmail.com>
2019-10-11 08:16:15 +0200

    All the changes

---
+++ AutoLock/Service1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoLock
+{
+    public partial class Service1 : ServiceBase
+    {
+        [DllImport(@"User32", SetLastError = true, EntryPoint = "RegisterPowerSettingNotification", CallingConvention = CallingConvention.StdCall)]
+        private static extern IntPtr RegisterPowerSettingNotification(IntPtr hRecipient, ref Guid PowerSettingGuid, Int32 Flags);
+
+        internal struct POWERBROADCAST_SETTING
+        {
+            public Guid PowerSetting;
+            public uint DataLength;
+            public byte Data;
+        }
+
+        Guid GUID_LIDSWITCH_STATE_CHANGE = new Guid(0xBA3E0F4D, 0xB817, 0x4094, 0xA2, 0xD1, 0xD5, 0x63, 0x79, 0xE6, 0xA0, 0xF3);
+        const int DEVICE_NOTIFY_WINDOW_HANDLE = 0x00000000;
+        const int WM_POWERBROADCAST = 0x0218;
+        const int PBT_POWERSETTINGCHANGE = 0x8013;
+
+        private bool? _previousLidState = null;
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            RegisterForPowerNotifications();
+            IntPtr hwnd = new WindowInteropHelper(this).Handle;
+            HwndSource.FromHwnd(hwnd).AddHook(new HwndSourceHook(WndProc));
+        }
+
+        protected override void OnStop()
+        {
+
+        }
+
+        private void RegisterForPowerNotifications()
+        {
+            IntPtr hLIDSWITCHSTATECHANGE = RegisterPowerSettingNotification(ServiceHandle,
+                 ref GUID_LIDSWITCH_STATE_CHANGE,
+                 DEVICE_NOTIFY_WINDOW_HANDLE);
+        }
+
+        IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            switch (msg)
+            {
+                case WM_POWERBROADCAST:
+                    OnPowerBroadcast(wParam, lParam);
+                    break;
+                default:
+                    break;
+            }
+            return IntPtr.Zero;
+        }
+
+        private void OnPowerBroadcast(IntPtr wParam, IntPtr lParam)
+        {
+            if ((int)wParam == PBT_POWERSETTINGCHANGE)
+            {
+                POWERBROADCAST_SETTING ps = (POWERBROADCAST_SETTING)Marshal.PtrToStructure(lParam, typeof(POWERBROADCAST_SETTING));
+                IntPtr pData = (IntPtr)((int)lParam + Marshal.SizeOf(ps));
+                Int32 iData = (Int32)Marshal.PtrToStructure(pData, typeof(Int32));
+                if (ps.PowerSetting == GUID_LIDSWITCH_STATE_CHANGE)
+                {
+                    bool isLidOpen = ps.Data != 0;
+
+                    if (!isLidOpen == _previousLidState)
+                        LidStatusChanged(isLidOpen);
+
+                    _previousLidState = isLidOpen;
+                }
+            }
+        }
+
+        private void LidStatusChanged(bool isLidOpen)
+        {
+            if (isLidOpen)
+            {
+                //Do some action on lid open event
+                Debug.WriteLine("{0}: Lid opened!", DateTime.Now);
+            }
+            else
+            {
+                //Do some action on lid close event
+                Debug.WriteLine("{0}: Lid closed!", DateTime.Now);
+            }
+        }
+    }
+}
---
+++ AutoLockTest/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoLockTest
+{
+    class Program
+    {
+        [DllImport(@"User32", SetLastError = true, EntryPoint = "RegisterPowerSettingNotification", CallingConvention = CallingConvention.StdCall)]
+        private static extern IntPtr RegisterPowerSettingNotification(IntPtr hRecipient, ref Guid PowerSettingGuid, Int32 Flags);
+
+        internal struct POWERBROADCAST_SETTING
+        {
+            public Guid PowerSetting;
+            public uint DataLength;
+            public byte Data;
+        }
+
+        Guid GUID_LIDSWITCH_STATE_CHANGE = new Guid(0xBA3E0F4D, 0xB817, 0x4094, 0xA2, 0xD1, 0xD5, 0x63, 0x79, 0xE6, 0xA0, 0xF3);
+        const int DEVICE_NOTIFY_WINDOW_HANDLE = 0x00000000;
+        const int WM_POWERBROADCAST = 0x0218;
+        const int PBT_POWERSETTINGCHANGE = 0x8013;
+
+        private bool? _previousLidState = null;
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+        }
+
+        public Program()
+        {
+            RegisterForPowerNotifications();
+            IntPtr hwnd = Process.GetCurrentProcess().MainWindowHandle;
+            HwndSource.FromHwnd(hwnd).AddHook(new HwndSourceHook(WndProc));
+        }
+
+        private void RegisterForPowerNotifications()
+        {
+            IntPtr handle = Process.GetCurrentProcess().MainWindowHandle;
+            IntPtr hLIDSWITCHSTATECHANGE = RegisterPowerSettingNotification(handle,
+                 ref GUID_LIDSWITCH_STATE_CHANGE,
+                 DEVICE_NOTIFY_WINDOW_HANDLE);
+        }
+
+        IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            switch (msg)
+            {
+                case WM_POWERBROADCAST:
+                    OnPowerBroadcast(wParam, lParam);
+                    break;
+                default:
+                    break;
+            }
+            return IntPtr.Zero;
+        }
+
+        private void OnPowerBroadcast(IntPtr wParam, IntPtr lParam)
+        {
+            if ((int)wParam == PBT_POWERSETTINGCHANGE)
+            {
+                POWERBROADCAST_SETTING ps = (POWERBROADCAST_SETTING)Marshal.PtrToStructure(lParam, typeof(POWERBROADCAST_SETTING));
+                IntPtr pData = (IntPtr)((int)lParam + Marshal.SizeOf(ps));
+                Int32 iData = (Int32)Marshal.PtrToStructure(pData, typeof(Int32));
+                if (ps.PowerSetting == GUID_LIDSWITCH_STATE_CHANGE)
+                {
+                    bool isLidOpen = ps.Data != 0;
+
+                    if (!isLidOpen == _previousLidState)
+                        LidStatusChanged(isLidOpen);
+
+                    _previousLidState = isLidOpen;
+                }
+            }
+        }
+
+        private void LidStatusChanged(bool isLidOpen)
+        {
+            if (isLidOpen)
+            {
+                //Do some action on lid open event
+                Debug.WriteLine("{0}: Lid opened!", DateTime.Now);
+            }
+            else
+            {
+                //Do some action on lid close event
+                Debug.WriteLine("{0}: Lid closed!", DateTime.Now);
+            }
+        }
+    }
+}
